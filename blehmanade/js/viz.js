ba03c787126d20cd20e902fbb5aaa86aa9a43109
blehman <the.lehman.report@gmail.com>
2016-01-13 21:53:04 -0700

    working version of line gradient

--- blehmanade/js/viz.js
+++ blehmanade/js/viz.js
@@ -1,4 +1,3 @@
-
 var chart = timeSeriesChart()
 
 d3.tsv("data/data.tsv", function(data) {
@@ -57,22 +56,22 @@ function timeSeriesChart() {
           ;
 
       // create gradient that will be referenced in css
-    svg.append("linearGradient")				
-        .attr("id", "line-gradient")			
-        .attr("gradientUnits", "userSpaceOnUse")	
-        .attr("x1", 0).attr("y1", yScale.range()[0])			
-        .attr("x2", 0).attr("y2", yScale.range()[1])		
+    svg.append("linearGradient")
+        .attr("id", "line-gradient")
+        .attr("gradientUnits", "userSpaceOnUse")
+        .attr("x1", 0).attr("y1", yScale.range()[0])
+        .attr("x2", 0).attr("y2", yScale.range()[1])
     .selectAll("stop")
         .data([
-            {offset: "0%", color: "red"},		
-            {offset: "40%", color: "red"},	
-            {offset: "40%", color: "black"},		
-            {offset: "62%", color: "black"},		
-            {offset: "62%", color: "lawngreen"},	
-            {offset: "100%", color: "lawngreen"}	
-        ])					
-    .enter().append("stop")			
-        .attr("offset", function(d) { return d.offset; })	
+            {offset: "0%", color: "red"},
+            {offset: "40%", color: "red"},
+            {offset: "40%", color: "black"},
+            {offset: "62%", color: "black"},
+            {offset: "62%", color: "lawngreen"},
+            {offset: "100%", color: "lawngreen"}
+        ])
+    .enter().append("stop")
+        .attr("offset", function(d) { return d.offset; })
         .attr("stop-color", function(d) { return d.color; });    });
   }
 
